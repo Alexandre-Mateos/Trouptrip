894f75c0cb6150f62c9fa66d58e93374ec91a86f
alexandre <mateosalexandre95@gmail.com>
2026-05-08 20:45:45 +0200

    change index.php

--- app_backend/public/index.php
+++ app_backend/public/index.php
@@ -2,19 +2,8 @@
 
 use App\Kernel;
 
-require_once dirname(__DIR__).'/vendor/autoload.php';
+require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
-$env = $_SERVER['APP_ENV']
-    ?? $_ENV['APP_ENV']
-    ?? getenv('APP_ENV')
-    ?? 'prod';
-
-$debug = filter_var(
-    $_SERVER['APP_DEBUG']
-    ?? $_ENV['APP_DEBUG']
-    ?? getenv('APP_DEBUG')
-    ?? false,
-    FILTER_VALIDATE_BOOL
-);
-
-return new Kernel($env, $debug);
+return static function (array $context) {
+    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+};
