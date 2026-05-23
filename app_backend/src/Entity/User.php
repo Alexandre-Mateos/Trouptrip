47bc8edeca3788910ab2f1bcec8017f3f9647262
alexandre <mateosalexandre95@gmail.com>
2026-05-23 16:52:34 +0200

    #12: (feature) install symfony packages for mail functions. Add malpit service in docker-compose for dev. Add UserToken entity and update User for verify mail feature and reset password feature

--- app_backend/src/Entity/User.php
+++ app_backend/src/Entity/User.php
@@ -94,10 +94,14 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Column(nullable: true)]
     private ?\DateTimeImmutable $updatedAt = null;
 
+    #[ORM\Column]
+    private ?bool $isVerified = null;
+
     public function __construct()
     {
         $this->createdAt = new \DateTimeImmutable();
         $this->roles = ['ROLE_USER'] ;
+        $this->isVerified = false;
 
         $this->trips = new ArrayCollection();
         $this->participations = new ArrayCollection();
@@ -412,6 +416,18 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     public function setUpdatedAt(?\DateTimeImmutable $updatedAt): static
     {
         $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    public function isVerified(): ?bool
+    {
+        return $this->isVerified;
+    }
+
+    public function setIsVerified(bool $isVerified): static
+    {
+        $this->isVerified = $isVerified;
 
         return $this;
     }
