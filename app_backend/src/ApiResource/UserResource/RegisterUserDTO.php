efa821d223239014479edb189de2807324fe516f
alexandre <mateosalexandre95@gmail.com>
2026-05-26 09:57:34 +0200

    #8: (feature): add constraints on RegisterUserDTO.php to ensure valid data before backend treatments

--- app_backend/src/ApiResource/UserResource/RegisterUserDTO.php
+++ app_backend/src/ApiResource/UserResource/RegisterUserDTO.php
@@ -2,15 +2,34 @@
 
 namespace App\ApiResource\UserResource;
 
-use App\Entity\User;
-use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Validator\Constraints as Assert;
 
 readonly class RegisterUserDTO
 {
     public function __construct(
+        #[Assert\NotBlank]
+        #[Assert\Email(message: 'L\'adresse email n\'est pas valide')]
         public ?string $email,
+
+        #[Assert\NotBlank]
+        #[Assert\NotCompromisedPassword]
+        #[Assert\PasswordStrength(
+            message: 'Le mot de passe n\'est pas assez fort'
+        )]
         public ?string $plainPassword,
+
+        #[Assert\NotBlank]
+        #[Assert\Length(
+            max: 50,
+            maxMessage: '50 caractères autorisés',
+        )]
         public ?string $firstname,
+
+        #[Assert\NotBlank]
+        #[Assert\Length(
+            max: 50,
+            maxMessage: '50 caractères autorisés',
+        )]
         public ?string $lastname
     )
     {
