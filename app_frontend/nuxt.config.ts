fe342b77913dff3f6c08d7fd15cdfc871f1d18c9
alexandre <mateosalexandre95@gmail.com>
2026-06-04 22:55:22 +0200

    #16 : (refacto) install tailwind css

--- app_frontend/nuxt.config.ts
+++ app_frontend/nuxt.config.ts
@@ -1,39 +1,44 @@
 export default defineNuxtConfig({
-    compatibilityDate: '2025-07-15',
-    devtools: { enabled: true },
-    ssr: false,
-    typescript: {
-        typeCheck: true,
-    },
-    runtimeConfig: {
-        // Dev : Nuxt utilisera cette URL par défaut
-        // Prod : Nuxt utilisera la variable d'environnement injectée par Docker
-        public: {
-            apiBaseUrl: 'http://localhost:8000/api'
-        }
-    },
-    vite: {
-        server: {
-            watch: {
-                usePolling: true,
-                interval: 100, // Vérifie toutes les 100ms
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+  ssr: false,
 
-                // AJOUTS POUR STABILISER WSL2 & DOCKER :
-                // Laisse 100ms de répit pour s'assurer que VS Code (Windows) a fini
-                // d'écrire le fichier dans WSL avant que Vite ne tente de compiler.
-                awaitWriteFinish: {
-                    stabilityThreshold: 100,
-                    pollInterval: 100
-                },
-                // Évite que le watcher ne scanne les fichiers générés par Nuxt,
-                // ce qui peut provoquer des boucles de rafraîchissement et saturer l'IPC.
-                ignored: [
-                    '**/.nuxt/**',
-                    '**/.output/**',
-                    '**/node_modules/**',
-                    '**/.git/**'
-                ]
-            }
-        }
-    }
+  typescript: {
+      typeCheck: true,
+  },
+
+  runtimeConfig: {
+      // Dev : Nuxt utilisera cette URL par défaut
+      // Prod : Nuxt utilisera la variable d'environnement injectée par Docker
+      public: {
+          apiBaseUrl: 'http://localhost:8000/api'
+      }
+  },
+
+  vite: {
+      server: {
+          watch: {
+              usePolling: true,
+              interval: 100, // Vérifie toutes les 100ms
+
+              // AJOUTS POUR STABILISER WSL2 & DOCKER :
+              // Laisse 100ms de répit pour s'assurer que VS Code (Windows) a fini
+              // d'écrire le fichier dans WSL avant que Vite ne tente de compiler.
+              awaitWriteFinish: {
+                  stabilityThreshold: 100,
+                  pollInterval: 100
+              },
+              // Évite que le watcher ne scanne les fichiers générés par Nuxt,
+              // ce qui peut provoquer des boucles de rafraîchissement et saturer l'IPC.
+              ignored: [
+                  '**/.nuxt/**',
+                  '**/.output/**',
+                  '**/node_modules/**',
+                  '**/.git/**'
+              ]
+          }
+      }
+  },
+
+  modules: ['@nuxtjs/tailwindcss']
 })
