7686d73408c8087383d4eefa2f3641823dba6387
alexandre <mateosalexandre95@gmail.com>
2026-07-24 08:20:55 +0200

    #38: (fix) fix modal error and authorize dismiss on outside click

--- app_frontend/app/components/BaseModal.vue
+++ app_frontend/app/components/BaseModal.vue
@@ -1,33 +1,21 @@
-<script lang="ts">
-import { defineComponent } from 'vue'
-
-export default defineComponent({
-  name: "BaseModal",
-  inheritAttrs: false,
-  props: {
-    open: {
-      type: Boolean,
-      default: false
-    }
-  }
+<script setup lang="ts">
+defineOptions({
+  inheritAttrs: false
 })
+
+const open = defineModel<boolean>('open', { default: false })
 </script>
 
 <template>
-
-  <UModal v-model:open="open"
-          :close="false"
-          :ui="{
-            body: 'bg-white border-none ring-0'
-          }"
+  <UModal
+      v-model:open="open"
+      :close="false"
+      :ui="{
+        body: 'bg-white border-none ring-0'
+      }"
   >
     <template #body>
-      <slot></slot>
+      <slot />
     </template>
   </UModal>
-
 </template>
-
-<style scoped>
-
-</style>
