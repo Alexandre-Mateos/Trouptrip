3e036de777855290647d097bd5a202a2c12be48b
alexandre <mateosalexandre95@gmail.com>
2026-08-04 13:19:57 +0200

    dev: (feat) test ci/cd

--- app_frontend/app/pages/index.vue
+++ app_frontend/app/pages/index.vue
@@ -9,7 +9,7 @@ export default defineComponent({
 <template>
   <h1>Bienvenue sur Trouptrip</h1>
   <p>L'application pour organiser vos voyages en groupe.</p>
-  <p>Attention, ceci est une version en cours de développement</p>
+  <p>Attention, ceci est une version en cours de développement. Certaines fonctionnalités peuvent ne pas être encore disponibles</p>
 </template>
 
 <style scoped>
