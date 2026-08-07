bb957a256a5bbf9f5ccda208c5346efbbfa6c233
alexandre <mateosalexandre95@gmail.com>
2026-08-07 09:55:46 +0200

    #29: (fix) add placeholder message when there is no invitation to display

--- app_frontend/app/pages/invitations.vue
+++ app_frontend/app/pages/invitations.vue
@@ -1,11 +1,11 @@
 <script lang="ts">
-import {defineComponent} from 'vue'
+import { defineComponent } from 'vue'
 import InvitationCard from "~/components/InvitationCard.vue";
-import type {IInvitation} from "~/interfaces/participation/i-invitation";
+import type { IInvitation } from "~/interfaces/participation/i-invitation";
 
 export default defineComponent({
   name: "invitations",
-  components: {InvitationCard},
+  components: { InvitationCard },
   data() {
     return {
       error: '',
@@ -14,56 +14,77 @@ export default defineComponent({
     }
   },
   methods: {
-    toggleDeclineInvitationModal(invitation: IInvitation){
-      if(!this.isDeclineTripModalOpen){
+    toggleDeclineInvitationModal(invitation?: IInvitation | null) {
+      if (!this.isDeclineTripModalOpen && invitation) {
         this.activeInvitation = invitation;
       }
       this.isDeclineTripModalOpen = !this.isDeclineTripModalOpen;
+
+      if (!this.isDeclineTripModalOpen) {
+        this.activeInvitation = null;
+      }
     },
-    declineInvitation(){
-      if(!this.activeInvitation){
+    async declineInvitation() {
+      if (!this.activeInvitation) {
         return;
       }
-      try{
-        this.participationStore.declineInvitation(this.activeInvitation?.id);
-      } catch {
-        this.error = 'Une erreur est survenue merci de réessayer plus tard';
-      }finally{
-        this.participationStore.removeInvitation(this.activeInvitation?.id);
-        this.toggleDeclineInvitationModal(this.activeInvitation);
+
+      const invitationId = this.activeInvitation.id;
+
+      try {
+        await this.participationStore.declineInvitation(invitationId);
+        this.participationStore.removeInvitation(invitationId);
+        this.toggleDeclineInvitationModal();
+      } catch (e) {
+        this.error = 'Une erreur est survenue, merci de réessayer plus tard.';
       }
     }
   },
   computed: {
-    participationStore(){
-      return useParticipationStore()
+    participationStore() {
+      return useParticipationStore();
+    },
+    hasInvitations() {
+      return this.participationStore.invitations.size > 0;
     }
   },
   async mounted() {
     try {
       await this.participationStore.fetchInvitations();
     } catch (e) {
-      this.error = 'impossible d\'afficher la lise des invitations pour le moment';
+      this.error = 'Impossible d\'afficher la liste des invitations pour le moment.';
     }
   }
 })
 </script>
 
 <template>
-  <InvitationCard v-for="invitation in participationStore.invitationList" :key="invitation.id" :invitation="invitation" @decline="toggleDeclineInvitationModal(invitation)"></InvitationCard>
+  <template v-if="hasInvitations">
+    <InvitationCard
+        v-for="invitation in participationStore.invitationList"
+        :key="invitation.id"
+        :invitation="invitation"
+        @decline="toggleDeclineInvitationModal(invitation)"
+    />
+  </template>
+
+  <div v-else class="empty-state">
+    <p>Rien à l'horizon pour l'instant !</p>
+    <p>Et si c'était vous qui preniez les devants en organisant la prochaine escapade ?</p>
+  </div>
 
   <BaseModal :open="isDeclineTripModalOpen">
     <div>
-      <p>Vous Etes sur le point de décliner l'invitation de {{ activeInvitation?.invitedBy.firstname }} pour le voyage suivant :</p>
-      <p>{{ activeInvitation?.trip.title }}</p>
-      <p>Etes vous sûr de vouloir continuer ?</p>
-      <div class="flex flex-row justify-center gap-2">
-        <DeleteButton icon="mdi:ban" label="Décliner" @click="declineInvitation"></DeleteButton>
-        <CancelButton @click="toggleDeclineInvitationModal"></CancelButton>
+      <p>Vous êtes sur le point de décliner l'invitation de {{ activeInvitation?.invitedBy.firstname }} pour le voyage suivant :</p>
+      <p class="font-bold">{{ activeInvitation?.trip.title }}</p>
+      <p>Êtes-vous sûr de vouloir continuer ?</p>
+
+      <div class="flex flex-row justify-center gap-2 mt-4">
+        <DeleteButton icon="mdi:ban" label="Décliner" @click="declineInvitation" />
+        <CancelButton @click="toggleDeclineInvitationModal" />
       </div>
     </div>
   </BaseModal>
-
 </template>
 
 <style scoped>
